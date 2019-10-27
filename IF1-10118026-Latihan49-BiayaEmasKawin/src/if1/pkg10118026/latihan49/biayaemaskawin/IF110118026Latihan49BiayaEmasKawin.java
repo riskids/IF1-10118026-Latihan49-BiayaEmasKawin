305de33b4140435d02f5a779badee60657a8567d
Riski Dwi Sabariyanto <bintangmalamhari@gmail.com>
2019-10-27 12:24:59 +0700

    Add files via upload

---
+++ IF1-10118026-Latihan49-BiayaEmasKawin/src/if1/pkg10118026/latihan49/biayaemaskawin/IF110118026Latihan49BiayaEmasKawin.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118026.latihan49.biayaemaskawin;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+/**
+ *
+ * @author 
+ * NAMA  : Riski Dwi Sabariyanto   
+ * KELAS : IF 1
+ * NIM   : 10118026
+ * Deskripsi Program : Program ini untuk membuat perhitungan untuk mas kawin.
+ */
+public class IF110118026Latihan49BiayaEmasKawin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        DecimalFormat mataUangIndonesia = (DecimalFormat) 
+                      DecimalFormat.getCurrencyInstance();
+        DecimalFormatSymbols formatRP = new DecimalFormatSymbols();
+        
+        formatRP.setCurrencySymbol("Rp.");
+        formatRP.setMonetaryDecimalSeparator(',');
+        formatRP.setGroupingSeparator('.');
+        
+        mataUangIndonesia.setDecimalFormatSymbols(formatRP);
+        
+        
+        Emas objEmas = new Emas();
+        objEmas.setHargaGram(570000);
+        objEmas.setBeratEmas(15.7);
+        System.out.println("====Aplikasi Hitung Harga Emas====");
+        System.out.printf("Total Bayar Emas : %s %n ", 
+                           mataUangIndonesia.format(
+                           objEmas.perhitungan(objEmas.getHargaGram(), 
+                           objEmas.getBeratEmas())));
+
+    }
+    
+}
